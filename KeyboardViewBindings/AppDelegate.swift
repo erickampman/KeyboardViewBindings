c34f6cf62114c50d256c66a4243f6ee83b766c26
Eric Kampman <erickampman@yahoo.com>
2016-12-30 11:11:37 -0800

    Initial Commit

---
+++ KeyboardViewBindings/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  KeyboardViewBindings
+//
+//  Created by Eric Kampman on 12/30/16.
+//  Copyright © 2016 Eric Kampman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
